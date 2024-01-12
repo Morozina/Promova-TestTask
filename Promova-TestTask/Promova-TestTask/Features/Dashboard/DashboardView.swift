e707d249ef14825a60f761a059b5c653e54a35d0
Morozina <zaryadina2014@gmail.com>
2024-01-12 21:57:05 +0100

    Created project

---
+++ Promova-TestTask/Promova-TestTask/Features/Dashboard/DashboardView.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardView.swift
+//  Promova-TestTask
+//
+//  Created by Vladyslav Moroz on 12/01/2024.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    DashboardView()
+}
---
+++ Promova-TestTask/Promova-TestTask/Features/MainView/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  MainView.swift
+//  Promova-TestTask
+//
+//  Created by Vladyslav Moroz on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MainView: App {
+    var body: some Scene {
+        WindowGroup {
+            DashboardView()
+        }
+    }
+}
