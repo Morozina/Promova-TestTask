fc8feb4285c2515417a40deefe1e9fb3a37ff049
Morozina <zaryadina2014@gmail.com>
2024-01-13 01:36:15 +0100

    Clean up

--- Promova-TestTask/Promova-TestTask/Application/Theme/Theme+Constants.swift
+++ Promova-TestTask/Promova-TestTask/Application/Theme/Theme+Constants.swift
@@ -9,10 +9,13 @@ import Foundation
 
 extension Theme {
     enum Constants {
+        /// Default corner radius size
         static let defaultCornerRadius: CGFloat = 6
 
+        /// Small shadow opacity 20%
         static let smallShadowOpacity: Double = 0.2
 
+        /// Half shadow opacity 50%
         static let halfShadowOpacity: Double = 0.5
 
         enum CategoryCard {
